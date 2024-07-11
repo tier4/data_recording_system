45a61904793c675cc27a45b14ad8bdfae5cd0c17
Manato HIRABAYASHI <manato.hirabayashi@tier4.jp>
2024-07-11 10:03:23 +0900

    fix: update build.sh to build necessary packages only
    Signed-off-by: Manato HIRABAYASHI <manato.hirabayashi@tier4.jp>

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 colcon build \
-    --symlink-install --continue-on-error --cmake-args -DCMAKE_BUILD_TYPE=Release
+    --symlink-install --continue-on-error --cmake-args -DCMAKE_BUILD_TYPE=Release \
+    --packages-up-to drs_launch
+
