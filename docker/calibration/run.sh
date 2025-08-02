aefc6a17e178dfc187cfd70425ba2dd5c39325c6
Shinnosuke Hirakawa <shinnosuke.hirakawa@tier4.jp>
2025-08-03 00:21:54 +0900

    feat: add Dockerfile and build scripts for DRS calibration

---
+++ docker/calibration/run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+set -e
+
+docker run -it --rm \
+    --net=host \
+    --gpus all \
+    -e LOCAL_UID=$(id -u) -e LOCAL_GID=$(id -g) -e LOCAL_USER=$(id -un) -e LOCAL_GROUP=$(id -gn) \
+    -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix \
+    -e XAUTHORITY=${XAUTHORITY} \
+    -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
+    -e NVIDIA_DRIVER_CAPABILITIES=all \
+    -v /etc/localtime:/etc/localtime:ro \
+    tier4/drs-calibration:latest "$@"
