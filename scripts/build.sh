2fe8217074ea5b0ea2d8607940e2e2c9cbd7475d
otenim <mineto.tsukada@gmail.com>
2026-02-02 21:13:07 +0900

    feat: cap colcon build parallel workers at 4 (#90)

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-colcon build --merge-install --install-base /opt/drs/install --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo --packages-up-to drs_launch
+colcon build --merge-install --install-base /opt/drs/install --parallel-workers 4 --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo --packages-up-to drs_launch
